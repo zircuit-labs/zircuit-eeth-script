da960edc17131a201bd3feaba945dcb680af59ae
Kacper Bak <kacper@quantstamp.com>
2024-03-30 06:33:06 -0700

    lowercase

--- src/consts.ts
+++ src/consts.ts
@@ -20,20 +20,20 @@ export const PENDLE_POOL_ADDRESSES = {
     LP: "0xe26d7f9409581f606242300fbfe63f56789f2169",
     START_BLOCK: 19516857,
     TREASURY: "0x8270400d528c34e1596ef367eedec99080a1b592",
-    EQB_STAKING: "0xb68BBA67C943665343D388302AA4c82754d4BD34",
-    PENPIE_RECEIPT_TOKEN: "0xEB808B323d8B9f2e2424B83e8a08c5F2633c9254",
+    EQB_STAKING: "0xb68bba67c943665343d388302aa4c82754d4bd34",
+    PENPIE_RECEIPT_TOKEN: "0xeb808b323d8b9f2e2424b83e8a08c5f2633c9254",
     // STAKEDAO_RECEIPT_TOKEN: "0xdd9df6a77b4a4a07875f55ce5cb6b933e52cb30a",
     MULTICALL: "0xca11bde05977b3631167028862be2a173976ca11",
     LIQUID_LOCKERS: [
         {
             // Penpie
             address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-            receiptToken: "0xEB808B323d8B9f2e2424B83e8a08c5F2633c9254",
+            receiptToken: "0xeb808b323d8b9f2e2424b83e8a08c5f2633c9254",
         },
         {
             // EQB
             address: '0x64627901dadb46ed7f275fd4fc87d086cff1e6e3',
-            receiptToken: "0xb68BBA67C943665343D388302AA4c82754d4BD34",
+            receiptToken: "0xb68bba67c943665343d388302aa4c82754d4bd34",
         },
         // {   // STAKEDAO
         //     address: '0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a',
