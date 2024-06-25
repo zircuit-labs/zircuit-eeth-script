b6262fa974fd7b5f8150883a5120479aec54b0f7
junochiu <ching.lun0216@gmail.com>
2024-06-25 09:28:02 -0400

    fix timestamp UTC

--- src/consts.ts
+++ src/consts.ts
@@ -40,12 +40,12 @@ export const PENDLE_POOL_ADDRESSES = {
   ],
 };
 
-export const V1_END_TIMESTAMP = 1719446399n; // 2024-06-26 23:59:59
+export const V1_END_TIMESTAMP = 1719446399n; // 2024-06-26 23:59:59 UTC
 
 export const MULTIPLIERS = {
   campaign: {
-    startTimestamp: 1719201600n, // 2024-06-24 00:00:00
-    endTimestamp: 1720670399n, // 2024-07-10 23:59:59
+    startTimestamp: 1719187200n, // 2024-06-24 00:00:00 UTC
+    endTimestamp: 1720655999n, // 2024-07-10 23:59:59 UTC
     multiplier: 200n,
   },
   multiplier: 150n,
