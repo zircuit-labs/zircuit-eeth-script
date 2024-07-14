0c6fd9c421c37940d2de4cdf8b9f675d0321c143
faycal <faycal@quantstamp.com>
2024-07-14 13:19:53 +0400

    minor change

--- src/consts.ts
+++ src/consts.ts
@@ -60,28 +60,31 @@ export const MULTIPLIER_TIMELINE = [
     }
 ]
 
+const PENPIE_RECEIPT_TOKEN = "0x5ed63a582533ad98bf15bc8669607eea5a61398b"
+const EQB_RECEIPT_TOKEN = "0x518ba876c34c688dd7a3d161602973a7705bbff2"
+
 export const PENDLE_POOL_ADDRESSES = {
     SY: "0xd7df7e085214743530aff339afc420c7c720bfa7",
     YT: "0x323da63d354c9d79df927fd21ce5b97add3a50d9",
     LP: "0x6c269dfc142259c52773430b3c78503cc994a93e",
-    START_BLOCK_SY: 19516857,
+    START_BLOCK_SY: 19516872,
     START_BLOCK: 20158640,
     TREASURY: "0x8270400d528c34e1596ef367eedec99080a1b592",
-    EQB_STAKING: "0xb68bba67c943665343d388302aa4c82754d4bd34",
-    PENPIE_RECEIPT_TOKEN: "0xeb808b323d8b9f2e2424b83e8a08c5f2633c9254",
+    EQB_STAKING: EQB_RECEIPT_TOKEN,
+    PENPIE_RECEIPT_TOKEN: PENPIE_RECEIPT_TOKEN,
     MULTICALL: "0xca11bde05977b3631167028862be2a173976ca11",
     LIQUID_LOCKERS: [
         {
             // Penpie
             name: "PenPie",
             address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-            receiptToken: "0x5ed63a582533ad98bf15bc8669607eea5a61398b",
+            receiptToken: PENPIE_RECEIPT_TOKEN,
         },
         {
             // EQB
             name: "EQB",
             address: '0x64627901dadb46ed7f275fd4fc87d086cff1e6e3',
-            receiptToken: "0x518ba876c34c688dd7a3d161602973a7705bbff2",
+            receiptToken: EQB_RECEIPT_TOKEN,
         },
     ]
 }
